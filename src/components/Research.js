59ce256585df0772802308aef4830ed08b122c93
Zach Robers <zach.robers@gmail.com>
2024-12-27 23:20:08 -0500

    research section almost done - just need to add images

--- src/components/Research.js
+++ src/components/Research.js
@@ -52,8 +52,15 @@ const DynamicFeature = () => {
 // Individual research projects
 const projects = [
   {
-    title: "Hickey Lab",
-    description: "Exploring cell growth patterns and their implications in medical research.",
+    title: "Deep Learning Techniques for Tissue Generation",
+    description: (
+      <>
+        At the Hickey Lab within Duke University's Biomedical Engineering Department, I'm developing deep learning methods for generation tasks with spatial transcriptomics data. Leveraging a dataset of tissue segments at single-cellular resolution, my initial task is to build a model capable of extending tissues in two-dimensions, predicting both the location and type of cells in an outpainted region. So far, I've developed the underlying framework for data preprocessing, including the dimensional reduction of spatial transcriptomics datasets to increase the concentration of cells relative to empty space in tissue regions. I created new ways to tokenize this data, building tools to translate datasets to language format while preserving spatial context. For the lab's generative models, I'm actively contributing to the construction of both transformer-based and diffusion-based architectures. I've also developed a custom evaluation metric to determine model performance by comparing the similarity of generated data to a validation set of actual data. Our ultimate goal is to construct 3D representations of tissues by deploying our generative techniques to fill in the missing data between 2D samples.{' '}
+        <a href="https://sites.google.com/view/john-w-hickey/home" target="_blank" rel="noopener noreferrer">
+          Learn more about the Hickey Lab here.
+        </a>
+      </>
+    ),
     style: {
       backgroundImage: 'url(/images/cells.png)',
       backgroundSize: 'cover',
@@ -61,8 +68,15 @@ const projects = [
     },
   },
   {
-    title: "Fluids",
-    description: "Studying fluid dynamics and their applications in engineering.",
+    title: "Fluid Dynamics",
+    description: (
+      <>
+        Within the Duke Math Department, I'm part of a research group studying the interactions between fluids and inelastic solids. Motivated by a desire to study the phenomenon of iceberg calving in which a storm surge collides with a glacier causing an iceberg to break off, I've deployed a three-pronged approach of modeling, analysis, and numerical simulation to investigate the underlying dynamics behind a generalized version of such interactions. Our model consists of shallow water (governed by the Saint-Venant Equations) coupled to an incompressible solid (governed by the linear wave equation) where the boundary between the fluid and solid is allowed to freely move. The boundary conditions in this model follow from physical principles, namely the conservation of energy. I contributed to the development of a finite-difference based numerical simulation which adjusts the classical MacCormack Method to reconcile the moving boundary and transfer of energy between the fluid and solid domains. I also studied the linear stability of this system when faced with a perturbation, elucidating the kinds of forcing waves which can lead to instability.{' '}
+        <a href="https://math.duke.edu/mathplus/2024/interactions-between-fluids-and-elastic-solids" target="_blank" rel="noopener noreferrer">
+          Learn more here.
+        </a>
+      </>
+    ),
     style: {
       backgroundImage: 'url(/images/iceberg.jpg)',
       backgroundSize: 'cover',
@@ -71,7 +85,11 @@ const projects = [
   },
   {
     title: "Heat Transfer",
-    description: "Optimizing heat transfer systems for sustainable energy.",
+    description: (
+      <>
+        Inside the UNC-Chapel Hill Fluids Laboratory, I've led an effort to study the diffusion of heat across irregular geometries. Our first investigation looks at the dynamics of heat transfer across an 'L' shaped geometry in comparison to the rectangular counterpart with equal area and perimeter. My approach involves experimentation, analysis of the heat diffusion partial differential equation, and numerical simulation. The experimental arrangement involves specially-cut conductors of varying sizes attached to a heat source and monitored with probes and thermal cameras. Asymptotics guide the analytical investigation and finite element method serves as the basis of the numerics. The studies have produced novel insights on how heat travels around a bend and grounded these insights in theory. I presented this work at the American Physical Society Division of Fluid Dynamics Meeting in November 2023. I also presented at the Triangle Computational and Applied Mathematics Symposium, winning best talk and poster by an undergraduate.
+      </>
+    ),
     style: {
       backgroundImage: 'url(/images/metal.jpg)',
       backgroundSize: 'cover',
@@ -79,6 +97,7 @@ const projects = [
     },
   },
 ];
+
 
 
 
@@ -166,6 +185,18 @@ const Research = () => {
             borderLeft: '20px solid #fff',
           }}></div>
         </button>
+        <div style={styles.dotsContainer}>
+          {projects.map((_, index) => (
+            <span
+              key={index}
+              onClick={() => setCurrentSlide(index)}
+              style={{
+                ...styles.dot,
+                backgroundColor: index === currentSlide ? '#2e4075' : '#d4e3fc',
+              }}
+            ></span>
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -199,6 +230,7 @@ const styles = {
   contentSection: {
     padding: '2em',
     textAlign: 'center',
+    backgroundColor: '#d4e3fc',
   },
   quoteContainer: {
     position: 'relative',
@@ -206,14 +238,14 @@ const styles = {
     margin: '2em auto',
     maxWidth: '800px',
     textAlign: 'center',
-    color: '#2e4075',
-    backgroundColor: '#d4e3fc',
+    color: '#fff',
+    backgroundColor: '#2e4075',
     borderRadius: '8px',
     boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
   },
   quoteMarksTop: {
     fontSize: '8rem',
-    color: '#2e4075',
+    color: '#fff',
     position: 'absolute',
     top: '70px',
     left: '10px',
@@ -221,7 +253,7 @@ const styles = {
   },
   quoteMarksBottom: {
     fontSize: '8rem',
-    color: '#2e4075',
+    color: '#fff',
     position: 'absolute',
     bottom: '-140px',
     right: '10px',
@@ -231,21 +263,20 @@ const styles = {
     fontSize: '2rem',
     lineHeight: '1.8',
     margin: '2em 0',
-    color: '#2e4075',
+    color: '#fff',
   },
   quoteAuthor: {
     fontSize: '1rem',
     fontStyle: 'italic',
     marginTop: '1em',
-    color: '#2e4075',
+    color: '#fff',
   },
   paragraph: {
     fontSize: '1.2rem',
     lineHeight: '1.8',
     maxWidth: '800px',
     margin: '0 auto',
-    color: '#fff',
-    backgroundColor: '#2e4075',
+    color: '#2e4075',
   },
   slideshowContainer: {
     position: 'relative',
@@ -259,14 +290,13 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    color: '#fff',
+    color: '#2e4075',
     textAlign: 'center',
-    cursor: 'pointer',
     transition: 'transform 0.5s ease-in-out',
   },
   projectContent: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    padding: '2em',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    padding: '7em',
     borderRadius: '8px',
   },
   arrowLeft: {
@@ -278,16 +308,16 @@ const styles = {
     color: '#fff',
     border: 'none',
     borderRadius: '50%',
-    width: '60px', // Increased size
-    height: '60px', // Increased size
-    fontSize: '2rem', // Larger font size
+    width: '60px', 
+    height: '60px', 
+    fontSize: '2rem', 
     cursor: 'pointer',
     zIndex: 1000,
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', // Subtle shadow
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', 
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    transition: 'transform 0.2s, box-shadow 0.2s', // Smooth transitions
+    transition: 'transform 0.2s, box-shadow 0.2s', 
   },
   arrowRight: {
     position: 'absolute',
@@ -298,15 +328,31 @@ const styles = {
     color: '#fff',
     border: 'none',
     borderRadius: '50%',
-    width: '60px', // Increased size
-    height: '60px', // Increased size
-    fontSize: '2rem', // Larger font size
+    width: '60px', 
+    height: '60px', 
+    fontSize: '2rem', 
     cursor: 'pointer',
     zIndex: 1000,
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', // Subtle shadow
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', 
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    transition: 'transform 0.2s, box-shadow 0.2s', // Smooth transitions
+    transition: 'transform 0.2s, box-shadow 0.2s', 
+  },
+  dotsContainer: {
+    position: 'absolute',
+    bottom: '20px',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '10px',
+    zIndex: 1000,
+  },
+  dot: {
+    width: '15px',
+    height: '15px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease',
   },
 };
